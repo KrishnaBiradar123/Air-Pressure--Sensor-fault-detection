09e991a6a9c6ceebffe06991bac919cdc7107d68
KrishnaBiradar123 <kittybiradar123@gmail.com>
2023-01-31 11:35:09 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="kittybiradar123@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
